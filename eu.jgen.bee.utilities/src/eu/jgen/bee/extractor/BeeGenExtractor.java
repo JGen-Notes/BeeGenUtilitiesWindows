9a13241c17ecea730f9e50e6f1ee99306bc12152
ms <ms@10.211.55.10>
2020-08-31 20:37:37 +0200

    Promoting to version 0.5

--- eu.jgen.bee.utilities/src/eu/jgen/bee/extractor/BeeGenExtractor.java
+++ eu.jgen.bee.utilities/src/eu/jgen/bee/extractor/BeeGenExtractor.java
@@ -2,7 +2,7 @@ package eu.jgen.bee.extractor;
 
 public class BeeGenExtractor {
 	
-	public static String VERSION = "0.4";
+	public static String VERSION = "0.5";
 	public static String SCHEMA = "9.2.A6";
 
 }
